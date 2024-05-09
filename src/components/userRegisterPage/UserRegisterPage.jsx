28a5867ac88a745d16d61858185b07ce84627c3d
Emir <jojojo5766@gmail.com>
2024-05-09 18:42:51 +0600

    dz3

---
+++ src/components/userRegisterPage/UserRegisterPage.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, click } from "../../redux/actions";
+import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
+
+const UserRegisterPage = () => {
+  const dispatch = useDispatch();
+  const { title } = useSelector((state) => state.userReducer);
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [validated, setValidated] = useState(false);
+
+  const formValue = (event) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, email } = form;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const namePattern = /^[a-zA-Z]+$/;
+    if (!emailPattern.test(email) || !namePattern.test(name)) {
+      return alert("Please ,fill in all fields");
+    }
+    dispatch(addUser(form));
+    setValidated(true);
+  };
+
+  return (
+    <div>
+      <span>{title}</span>
+      <button onClick={() => dispatch(click())}>+</button>
+      <Form noValidate validated={validated} onSubmit={(e) => handleSubmit(e)}>
+        <Row>
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              required
+              name="name"
+              type="text"
+              placeholder="First name"
+              defaultValue=""
+              onChange={(e) => formValue(e)}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+            <Form.Label>E-mail</Form.Label>
+            <InputGroup hasValidation>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="example@gmail.com"
+                aria-describedby="inputGroupPrepend"
+                onChange={(e) => formValue(e)}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please write correct email.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom03">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => formValue(e)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Button type="submit" onClick={handleSubmit}>
+          Submit form
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default UserRegisterPage;
---
+++ src/redux/reducers/userReducer.jsx
@@ -0,0 +1,15 @@
+import { types } from "../types";
+
+const initialState = {
+  title: 0,
+  text: "",
+};
+
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.CLICK:
+      return { ...state, title: state.title + 1 };
+    default:
+      return state;
+  }
+}
